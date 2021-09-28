afc3dc6743f4c363b639ea899a49733dd50323cf
Julius Žaromskis <89458604+julius-welink@users.noreply.github.com>
2021-09-28 13:12:17 +0300

    Refactor according to comments

--- server/rate_counter_test.go
+++ server/rate_counter_test.go
@@ -1,3 +1,16 @@
+// Copyright 2012-2021 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package server
 
 import (
@@ -7,6 +20,7 @@ import (
 
 func TestRateCounter(t *testing.T) {
 	counter := newRateCounter(10)
+	counter.interval = 10 * time.Millisecond
 
 	var i int
 	for i = 0; i <= 10; i++ {
@@ -19,7 +33,7 @@ func TestRateCounter(t *testing.T) {
 		t.Errorf("Expected i = 10, got %d", i)
 	}
 
-	time.Sleep(1100 * time.Millisecond)
+	time.Sleep(11 * time.Millisecond)
 
 	if !counter.allow() {
 		t.Errorf("Expected true after current time window expired")
